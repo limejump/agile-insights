f094f9cc8937eaa3cd6f641021693622e015d110
Grahame Gardiner <grahame.gardiner@limejump.com>
2020-07-06 17:53:35 +0100

    pep8 import fixes

--- backends/jira/__init__.py
+++ backends/jira/__init__.py
@@ -1,4 +1,6 @@
-from .types import JiraIssue, IssueMetrics, DUMPFORMAT
+from .types import JiraIssue, DUMPFORMAT
 from .fetch import (
-    fetch_all_issues, fetch_sprints, get_latest_completed_sprint, ALL_ISSUES_FILENAME,
-    measurable_issue)
+    fetch_all_completed_issues,
+    fetch_sprints,
+    get_latest_completed_sprint,
+    ALL_ISSUES_FILENAME)
