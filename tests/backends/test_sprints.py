5d310d859fe1648811ff1f7eac3c34b7c9715e13
Grahame Gardiner <grahame.gardiner@limejump.com>
2020-10-01 16:53:16 +0100

    Add more sprint interpretation tests

--- tests/backends/test_sprints.py
+++ tests/backends/test_sprints.py
@@ -41,27 +41,132 @@ def sprint_issues_lens():
 
 
 @pytest.fixture
-def base_issue():
-    return JiraIssue(
-        name="An Issue",
-        summary="Issue for sprint",
-        epic=None,
-        type_=IssueTypes.story,
-        status=StatusTypes.todo,
-        story_points=5.0,
-        subtasks=[],
-        status_metrics=StatusMetrics(
-            started=False,
-            finished=False,
-            start=None,
-            end=None,
-            days_taken=None),
-        sprint_metrics=SprintMetrics(sprint_additions=[]))
+def base_issues():
+    return [
+        JiraIssue(
+            name="An Issue",
+            summary="Issue for sprint",
+            epic=None,
+            type_=IssueTypes.story,
+            status=StatusTypes.todo,
+            story_points=5.0,
+            subtasks=[],
+            status_metrics=StatusMetrics(
+                started=False,
+                finished=False,
+                start=None,
+                end=None,
+                days_taken=None),
+            sprint_metrics=SprintMetrics(sprint_additions=[]))
+    ]
 
 
 def test_sprint_with_issue(
-        basic_scenario, base_issue, sprint_issues_lens):
+        basic_scenario, base_issues, sprint_issues_lens):
     raw_json, result = basic_scenario
-    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issue)
-    result = sprint_issues_lens.set(base_issue)(result)
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    result = sprint_issues_lens.set(base_issues)(result)
     assert sprint == result
+
+
+def test_no_sprint_metrics_means_unplanned(basic_scenario, base_issues):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    assert sprint.planned_issue(sprint.issues[0]) is False
+
+
+def test_planned_issue_added_at_sprint_start(
+        basic_scenario, base_issues, sprint_issues_lens):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    sprint = sprint_issues_lens[0].sprint_metrics.set(
+        SprintMetrics(
+            sprint_additions=[{
+                "timestamp": datetime(
+                    2020, 1, 1, 9, 0, 0, tzinfo=timezone(timedelta(hours=1))),
+                "sprint_id": 1
+            }]
+        )
+    )(sprint)
+    assert sprint.planned_issue(sprint.issues[0]) is True
+
+
+def test_planned_issue_added_before_sprint_start(
+        basic_scenario, base_issues, sprint_issues_lens):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    sprint = sprint_issues_lens[0].sprint_metrics.set(
+        SprintMetrics(
+            sprint_additions=[{
+                "timestamp": datetime(
+                    2020, 1, 1, 8, 59, 0, tzinfo=timezone(timedelta(hours=1))),
+                "sprint_id": 1
+            }]
+        )
+    )(sprint)
+    assert sprint.planned_issue(sprint.issues[0]) is True
+
+
+def test_unplanned_issue_added_after_sprint_start(
+        basic_scenario, base_issues, sprint_issues_lens):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    sprint = sprint_issues_lens[0].sprint_metrics.set(
+        SprintMetrics(
+            sprint_additions=[{
+                "timestamp": datetime(
+                    2020, 1, 1, 9, 1, 0, tzinfo=timezone(timedelta(hours=1))),
+                "sprint_id": 1
+            }]
+        )
+    )(sprint)
+    assert sprint.planned_issue(sprint.issues[0]) is False
+
+
+def test_started_in_sprint(
+        basic_scenario, base_issues, sprint_issues_lens):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    sprint = sprint_issues_lens[0].status_metrics.set(
+        StatusMetrics(
+            started=False,
+            finished=True,
+            start=datetime(
+                2020, 1, 1, 9, 0, 0, tzinfo=timezone(timedelta(hours=1))),
+            end=datetime(
+                2020, 1, 2, 9, 0, 0, tzinfo=timezone(timedelta(hours=1))),
+            days_taken=1),
+    )(sprint)
+
+
+def test_started_because_finished_in_sprint(
+        basic_scenario, base_issues, sprint_issues_lens):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    sprint = sprint_issues_lens[0].status_metrics.set(
+        StatusMetrics(
+            started=False,
+            finished=True,
+            start=None,
+            end=datetime(
+                2020, 1, 2, 9, 0, 0, tzinfo=timezone(timedelta(hours=1))),
+            days_taken=1),
+    )(sprint)
+    assert sprint.started_in_sprint(sprint.issues[0]) is True
+
+
+def test_not_started_in_sprint(
+        basic_scenario, base_issues, sprint_issues_lens):
+    raw_json, _ = basic_scenario
+    sprint = Sprint.from_parsed_json(raw_json, lambda x: base_issues)
+    sprint = sprint_issues_lens[0].status_metrics.set(
+        StatusMetrics(
+            started=False,
+            finished=True,
+            start=datetime(
+                2019, 12, 30, 9, 0, 0, tzinfo=timezone(timedelta(hours=1))),
+            end=datetime(
+                2020, 1, 1, 8, 59, 0, tzinfo=timezone(timedelta(hours=1))),
+            days_taken=1),
+    )(sprint)
+    assert sprint.started_in_sprint(sprint.issues[0]) is False
