18e41dee7a97c5a69a4782ceb45d5cf1115e4d26
Grahame Gardiner <grahame.gardiner@limejump.com>
2020-10-15 14:38:07 +0100

    Leave dates as datetimes in to_json
    so that they are stored as ISOdate object in mongo which
    makes them easier to query

--- backends/jira/parse.py
+++ backends/jira/parse.py
@@ -314,8 +314,9 @@ class JiraIssue:
             "story_points": self.story_points,
             "started": self.status_metrics.started,
             "finished": self.status_metrics.finished,
-            "start_time": maybe_timestring(self.status_metrics.start),
-            "end_time": maybe_timestring(self.status_metrics.end),
+            # FIXME: start/end are datetimes so this isn't really json
+            "start_time": self.status_metrics.start,
+            "end_time": self.status_metrics.end,
             "days_taken": self.status_metrics.days_taken,
             "description": self.description,
             "bau": self.bau,
@@ -373,8 +374,9 @@ class Sprint:
             "name": self.name,
             "goal": self.goal,
             "state": self.state,
-            "start": maybe_timestring(self.start),
-            "end": maybe_timestring(self.end),
+            # FIXME: start/end are datetimes so this isn't really json
+            "start": self.start,
+            "end": self.end,
             "issues": issues}
 
     def _issue_to_json(self, issue: JiraIssue) -> List[dict]:
