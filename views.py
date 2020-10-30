18cf5389e2dac48459ca237cd968deef8e44a762
Grahame Gardiner <grahame.gardiner@limejump.com>
2020-10-30 16:45:04 +0000

    Remove sprint overview pie chart
    because it's too confusing

--- views.py
+++ views.py
@@ -31,7 +31,6 @@ class Sprints:
 
 
 class Sprint:
-    empty_sub_pie = go.Pie(labels=[], values=[], scalegroup='one')
     empty_pie = go.Pie(labels=[], values=[])
 
     def __init__(self, sprint_id, edit_notes):
@@ -51,23 +50,6 @@ class Sprint:
             # FIXME: may want to handle this using validation using
             # FormFeedback
             self.edit_notes = True
-
-    def _mk_sub_pie_trace(self, df, groupby, df_filters):
-        filtered = df[df_filters]
-
-        if filtered.empty:
-            return self.empty_sub_pie
-
-        pie = px.pie(
-            filtered.groupby(groupby).size().reset_index(
-                name='issue_count'),
-            values='issue_count',
-            names=groupby)
-        # can only make subplots from graph objects
-        return go.Pie(
-            labels=pie.data[0]['labels'],
-            values=pie.data[0]['values'],
-            scalegroup='one')
 
     def maybe_bau_breakdown(self):
         df = self.model.mk_bau_breakdown_df()
@@ -105,47 +87,6 @@ class Sprint:
         )
         return fig
 
-    def mk_overview_trace(self):
-        # FIXME: this 'view' manipulates the DataFrame
-        # breaking separation of concerns. Need to think
-        # about migrating some of this detail to the model
-        # interface.
-        mk_sub_pie = partial(
-            self._mk_sub_pie_trace, self.model.issues_df, 'description')
-        fig = make_subplots(
-            1, 4, specs=[[
-                {'type': 'domain'}, {'type': 'domain'},
-                {'type': 'domain'}, {'type': 'domain'}]],
-            subplot_titles=[
-                'Planned', 'Planned Delivered',
-                'Unplanned', 'Unplanned Delivered'])
-        fig.add_trace(
-            mk_sub_pie(
-                df_filters=self.model.issues_df.planned.eq(True)),
-            1, 1)
-        fig.add_trace(
-            mk_sub_pie(
-                df_filters=(
-                    self.model.issues_df.planned.eq(True) &
-                    self.model.issues_df.finished_in_sprint.eq(True))),
-            1, 2)
-        fig.add_trace(
-            mk_sub_pie(
-                df_filters=self.model.issues_df.planned.eq(False)),
-            1, 3)
-        fig.add_trace(
-            mk_sub_pie(
-                df_filters=(
-                    self.model.issues_df.planned.eq(False) &
-                    self.model.issues_df.finished_in_sprint.eq(True))),
-            1, 4)
-        fig.update_layout(
-            legend_x=1,
-            legend_y=1)
-        fig.update_traces(
-            textinfo='percent', showlegend=True)
-        fig.update_layout(transition_duration=500)
-        return fig
 
     @staticmethod
     def editable_notes(notes):
